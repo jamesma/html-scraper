a7318d28a615faa82cc4228cf9853dd4b5df7ac9
James Ma <jamesma.1987@gmail.com>
2015-08-07 22:25:07 +0800

    Update to usage yhat/scrape instead of jloup
    Changed default target to scrape from soundcloud to hackernews (using yhat/scrape).

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# HTML Scraper
-Scraps HTML websites using [jloup/html](https://github.com/jloup/html).
+HTML Scraper
+============
+Scraps HTML websites using [yhat/scrape](https://github.com/yhat/scrape).
+
+Usage
+=====
+```
+Usage: go main.go [flags]
+  -list=false: list all targets available
+  -target="hackernews": the target to scrap
+```
--- main.go
+++ main.go
@@ -3,23 +3,57 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/jamesma/html-scraper/scrap"
 )
 
 var (
-	target = flag.String("target", "soundcloud", "the target to scrap")
+	targets = []string{
+		"hackernews",
+		"soundcloud",
+		// "chamberorganizer", TODO
+	}
+
+	listTargets = flag.Bool("list", false, "list all targets available")
+	target      = flag.String("target", "hackernews", "the target to scrap")
 )
 
-func main() {
-	flag.Parse()
+func customUsageMessage() func() {
+	return func() {
+		fmt.Fprintf(os.Stderr, "Usage: go main.go [flags]\n")
+		flag.PrintDefaults()
+	}
+}
 
+func listAllTargets() {
+	fmt.Println("targets available:")
+	for _, target := range targets {
+		fmt.Println(target)
+	}
+}
+
+func scrapeTarget(target *string) {
 	switch *target {
+	case "hackernews":
+		scrap.HackerNews()
 	case "soundcloud":
 		scrap.Soundcloud()
-	// case "chamberorganizer":
+	// case "chamberorganizer": TODO
 	// 	scrap.ChamberOrganizer()
 	default:
 		fmt.Println("unsupported target:", *target)
 	}
+}
+
+func main() {
+	flag.Usage = customUsageMessage()
+	flag.Parse()
+
+	if *listTargets {
+		listAllTargets()
+		return
+	}
+
+	scrapeTarget(target)
 }
